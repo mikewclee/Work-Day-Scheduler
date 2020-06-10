7e0dea6aac47c7d571a28d9f75f6e6c976cd5f91
Michael W Lee <mikewclee@gmail.com>
2020-06-09 22:59:48 -0400

    updated hours colors, save to local storage, erase all events

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,45 +11,53 @@ $(document).ready(function () {
 
     //loop to setup timeblocks 9am to 5pm
     for (var i = 0; i < timeBlocks.length; i++) {
-        // console.log(timeBlocks[i]);
-        var newRow = $(`<div class = 'row'></div>`).attr({ id: "row" + (i) });
+        
+        var newRow = $(`<div class = 'row'></div>`).attr({ id: "row" + i});
         $('.container').append(newRow);
         var hourly = $(`<div class = 'col-2 time-block' mx auto>`).append(timeBlocks[i]);
-        var inputBox = $(`<input type='text' class='col-9' placeholder = "Add event"> </input>`).attr({ id: "inputBox" + (i)});
-        var saveBtn = $(`<button class='col-1 btn btn-success saveBtn'> Save </button>`).attr({ id: "button" + (i) });
+        var inputBox = $(`<div class='col-9 inputBoxHr'> </div>`).attr({ id: "inputBox" + i });
+        //hourly text box with saved items
+        var textArea = $("<textarea>").attr({class: "col-12", id: "textInputbutton" + i}).text(localStorage.getItem("myEventbutton" + i));
+        inputBox.append(textArea);
+        var saveBtn = $(`<button class='col-1 fas fa-save btn btn-success saveBtn'></button>`).attr({ id: "button" + i});
         $(newRow).append(hourly, inputBox, saveBtn);
 
         //compared 9am to current time.
         var currentTime = moment().hour();  //get current hour as integer
         var calendarTime = (9 + i);  //set calendar time to integer 9
-        // console.log("Current Hour: " + currentTime);
-        // console.log("Calendar time: " + calendarTime);
+        // console.log("CurrentTime: " + currentTime);
         if (currentTime < calendarTime) {
-            $("#inputBox" + (i)).addClass("future");
+            $("#inputBox" + i).addClass("future");
         }
         else if (currentTime > calendarTime) {
-            $("#inputBox" + (i)).addClass("past");
+            $("#inputBox" + i).addClass("past");
         }
         else {
-            $("#inputBox" + (i)).addClass("present");
+            $("#inputBox" + i).addClass("present");
         }
 
     };
 
     // add event to local storage 
-    $("button").click(function(){
+    $("button").click(function () {
         event.preventDefault();
-        var currentID = $(this).attr('id'); //button1
+        var currentID = $(this).attr('id'); 
         console.log(currentID);
-        var toStore = $(('#inputBox')).value; //get value from inputBox
+        var toStore = $(("#textInput" + currentID)).val(); //get value from textInputButton0
         console.log('store value =' + toStore);
         localStorage.setItem("myEvent" + currentID, toStore); //store value from inputBox
 
     });
 
+    //Clear all scheduler
+    var clearBtn =$('<button>ERASE ALL SCHEDULE</button>').addClass("btn btn-danger clearAll");
+    $('.lead').append(clearBtn);
+    $('.clearAll').click(function(){
+        for (var i=0; i< timeBlocks.length; i++) {
+            $('#textInputbutton'+i).text('');
+            localStorage.clear();
+        }
 
-    function clear() {
-        $("#inputBox").empty();
-      }
+    });
 
 });
