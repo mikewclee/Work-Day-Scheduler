31bee08f8dbf4ff7fcd26aa32737a13da4cc13ab
Michael W Lee <mikewclee@gmail.com>
2020-06-12 09:41:12 -0400

    js cleanup

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-Work Day Scheduler
+##  Work Day Scheduler
 
 This is a simple calendar application that allows the user to save events for each hour of the day from 9am to 5pm. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 The user types in events in the textbox corresponding to the relevant row hours and can save by clicking the save button on the far right. All events saved are stored locally and will remain in the text box until cleared or user click 'ERASE ALL SCHEDULE'.
 
+##  
 The current date and time are dynamically upated at the top using Moment.js. 
 Additionally, each row is dynamically updated by color based on the current time as follows:
 Grey: Past hours from current hour
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     // Set currentDay date and time 
     setInterval(function () {
         var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -9,17 +8,21 @@ $(document).ready(function () {
 
     var timeBlocks = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
-    //loop to setup timeblocks 9am to 5pm
+    //loop to setup and add timeblocks 9am to 5pm
     for (var i = 0; i < timeBlocks.length; i++) {
         
-        var newRow = $(`<div class = 'row'></div>`).attr({ id: "row" + i});
+        //new row
+        var newRow = $('<div>').addClass('row').attr({id: "row" + i});
         $('.container').append(newRow);
-        var hourly = $(`<div class = 'col-2 time-block' mx auto>`).append(timeBlocks[i]);
+        //new var to add hourly am 
+        var hourly = $('<div>').addClass('col-2 time-block mx auto').append(timeBlocks[i]);
+        //new var to Input box per hour
         var inputBox = $(`<div class='col-9 inputBoxHr'> </div>`).attr({ id: "inputBox" + i });
         //hourly text box with saved items
-        var textArea = $("<textarea>").attr({class: "col-12", id: "textInputbutton" + i}).text(localStorage.getItem("myEventbutton" + i));
+        var textArea = $('<textarea>').attr({class: "col-12", id: 'textInputbutton' + i}).text(localStorage.getItem('myEventbutton' + i));
         inputBox.append(textArea);
-        var saveBtn = $(`<button class='col-1 fas fa-save btn btn-success saveBtn'></button>`).attr({ id: "button" + i});
+        //new var save button
+        var saveBtn = $('<button>').addClass('col-1 fas fa-save btn btn-success saveBtn').attr({ id: "button" + i});
         $(newRow).append(hourly, inputBox, saveBtn);
 
         //compared 9am to current time.
@@ -27,26 +30,24 @@ $(document).ready(function () {
         var calendarTime = (9 + i);  //set calendar time to integer 9
         // console.log("CurrentTime: " + currentTime);
         if (currentTime < calendarTime) {
-            $("#inputBox" + i).addClass("future");
+            $("#inputBox" + i).addClass("future");  //time is in future, turn green
         }
         else if (currentTime > calendarTime) {
-            $("#inputBox" + i).addClass("past");
+            $("#inputBox" + i).addClass("past");   //time is in the past, turn grey
         }
         else {
-            $("#inputBox" + i).addClass("present");
+            $("#inputBox" + i).addClass("present");  //time is present, turn red
         }
-
     };
 
     // add event to local storage 
     $("button").click(function () {
         event.preventDefault();
         var currentID = $(this).attr('id'); 
-        console.log(currentID);
+        // console.log(currentID);
         var toStore = $(("#textInput" + currentID)).val(); //get value from textInputButton0
-        console.log('store value =' + toStore);
+        // console.log('store value =' + toStore);
         localStorage.setItem("myEvent" + currentID, toStore); //store value from inputBox
-
     });
 
     //Clear all scheduler
@@ -57,7 +58,6 @@ $(document).ready(function () {
             $('#textInputbutton'+i).text('');
             localStorage.clear();
         }
-
     });
 
 });
